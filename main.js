cb0e19395932559d167fbcb073c8ee06f5611a87
davemarch <davemarchaudio@gmail.com>
2020-03-21 16:26:31 +0000

    First commit

---
+++ main.js
@@ -0,0 +1,16 @@
+let dropdownActive = () => {
+    let dropdown = document.getElementById("dropdownID");
+    dropdown.classList.toggle('active'); 
+  }
+
+  let menuItemRemoveActive = () => {
+    let dropdown = document.getElementById("dropdownID");
+    dropdown.classList.toggle('active'); 
+  }
+
+  let burgerAnimation = () => {
+    let burger = document.getElementById("burger");
+    burger.classList.toggle('burger'); 
+  }
+
+
